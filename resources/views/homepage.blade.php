6f871c65366376bc9e9a1c6d3b13b0aa2137b53d
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2024-10-01 22:35:44 +0000

    refactored headlines

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,25 +1,22 @@
 <x-layout>
 
    <x-slot name="title">
-      New Title
+        Ekow Stones | Unique Stonework for Countertops, Walls, & More
    </x-slot>
 
    <x-slot name="content">
-    <x-nav/>
+      <x-nav />
+    <x-headline/>
 
       <section>
-        <div class="text-center text-5xl font-bold">
-            <h1 class="text-primary-700">Elevate your Space</h1>
-            <h2 class="text-primary-400 mt-2">With Timeless Stone</h2>
-        </div>
-      </section>
-
-      <section>
-        <div>
-        <div class="px-0 bg-primary-600 h-[834px] rounded-3xl flex overflow-hidden" style="background-image: url('https://place-hold.it/800x800'); background-size: cover; background-position: center;">
-            <div class="w-[475px] p-12 bg-primary-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti alias vero incidunt pariatur accusamus saepe aliquam magni porro, repellendus, harum consequuntur. Quis nobis, deleniti dolorum quo odit omnis necessitatibus quas.</div>
-        </div>
-    </div>
+         <div>
+            <div class="flex h-[834px] overflow-hidden rounded-3xl bg-primary-600 px-0"
+               style="background-image: url('https://place-hold.it/800x800'); background-size: cover; background-position: center;">
+               <div class="w-[475px] bg-primary-600 p-12">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Corrupti alias vero incidunt pariatur accusamus saepe aliquam magni porro, repellendus, harum
+                  consequuntur. Quis nobis, deleniti dolorum quo odit omnis necessitatibus quas.</div>
+            </div>
+         </div>
       </section>
    </x-slot>
 </x-layout>
