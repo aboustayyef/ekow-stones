4ceea3e6dd6e6e6c46a427cf999f63da1d44d1ca
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2024-10-01 12:14:11 +0000

    work in progress

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,22 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home Page (Alpha)</title>
-    @vite('resources/css/app.css')
-</head>
-<body>
-    <section>
-        <div class="flex justify-between items-center">
+<x-layout>
+
+   <x-slot name="title">
+      New Title
+   </x-slot>
+
+   <x-slot name="content">
+      <section>
+         <div class="flex items-center justify-between">
             <x-ui.logo width="180" height="55" />
             <ul id="desktopMenu" class="flex space-x-4">
-                <li><a href="#">About</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Contact Us</a></li>
+               <li><a href="#">About</a></li>
+               <li><a href="#">Services</a></li>
+               <li><a href="#">Contact Us</a></li>
             </ul>
+         </div>
+      </section>
+
+      <section>
+        <div class="text-center text-5xl font-bold">
+            <h1 class="text-primary-700">Elevate your Space</h1>
+            <h2 class="text-primary-400 mt-2">With Timeless Stone</h2>
         </div>
-    </section>
-</body>
-</html>
+      </section>
+
+      <section>
+        <div>
+        <div class="px-0 bg-primary-600 h-[834px] rounded-3xl flex overflow-hidden" style="background-image: url('https://place-hold.it/1200x700'); background-size: cover; background-position: center;">
+            <div class="w-[475px] p-12 bg-primary-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti alias vero incidunt pariatur accusamus saepe aliquam magni porro, repellendus, harum consequuntur. Quis nobis, deleniti dolorum quo odit omnis necessitatibus quas.</div>
+        </div>
+    </div>
+      </section>
+   </x-slot>
+</x-layout>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,33 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
+
 export default {
     content: [
-      "./resources/**/*.blade.php",
-      "./resources/**/*.js",
-      "./resources/**/*.vue",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
     ],
     theme: {
-      extend: {},
+        extend: {
+            colors: {
+                primary: {
+                    50: "#f2f7f3",
+                    100: "#dfecdf",
+                    200: "#c2d8c4",
+                    300: "#98bd9e",
+                    400: "#6c9b76",
+                    500: "#4b7e57",
+                    600: "#386343",
+                    700: "#2c5036",
+                    800: "#25402c",
+                    900: "#1f3525",
+                    950: "#111d15",
+                },
+            },
+            fontFamily: {
+                sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
     plugins: [],
-  }
+};
