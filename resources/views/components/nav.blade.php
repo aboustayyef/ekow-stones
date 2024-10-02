74d91b74d49698a591a0efa6790ed974a43987fd
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2024-10-02 12:06:14 +0000

    improvements to mobile menu

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -7,7 +7,7 @@
              <x-ui.close id="closebutton" class="h-8 w-8 hidden lg:hidden cursor-pointer hover:text-primary-400" />
          </div>
       </div>
-      <ul id="menu" class="hidden lg:flex lg:space-x-4 mt-8 lg:mt-0">
+      <ul id="menu" class="hidden p-4 lg:p-0 bg-primary-100 lg:bg-inherit lg:flex lg:space-x-4 mt-8 lg:mt-0">
          <li><a href="#">About</a></li>
          <li><a href="#">Services</a></li>
          <li><a href="#">Contact Us</a></li>
