93d28952604044eef5490b78329ad8dfa2668cf9
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2024-10-06 10:55:19 +0000

    build

--- resources/views/components/our-work.blade.php
+++ resources/views/components/our-work.blade.php
@@ -1,11 +1,11 @@
 <section class="mt-4">
     <div>
-        <h2 class="text-5xl text-primary-600 font-bold">Our Work</h2>
+        <h2 class="text-3xl lg:text-5xl text-primary-600 font-bold">Our Work</h2>
     </div>
 </section>
 <section class="py-0 border-b border-gray-400 ">
     <div>
-        <ul id="ourWork" class="flex space-x-12">
+        <ul id="ourWork" class="flex space-x-12 overflow-x-auto">
             <li class="selected"><a href="">Countertops</a></li>
             <li><a href="">Walls</a></li>
             <li><a href="">Granite</a></li>
@@ -15,5 +15,7 @@
     </div>
 </section>
 <section>
-    <div class="mt-4 w-full aspect-video bg-cover bg-center bg-no-repeat" style="background-image: url('/storage/img/unsplash-kitchen-homepage.jpg');"></div>
+    <div>
+        <div class="mt-4 w-full aspect-video bg-cover bg-center bg-no-repeat" style="background-image: url('/storage/img/unsplash-kitchen-homepage.jpg');"></div>
+    </div>
 </section>
