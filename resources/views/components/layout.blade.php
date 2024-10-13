f2fc97bd1d87bd9c6be2b8e2c1ea34f99c9c6b1e
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2024-10-13 22:16:05 +0000

    work in progress

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{$title}}</title>
-    @vite('resources/css/app.css')
+    <style>{!!Vite::content('resources/css/app.css')!!}</style>
     <!-- Google Fonts -->
    <link rel="preconnect" href="https://fonts.googleapis.com">
    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
--- resources/views/components/our-work.blade.php
+++ resources/views/components/our-work.blade.php
@@ -1,7 +1,10 @@
 <section class="mt-4" id="ourWorkSection">
-    <div>
-        <h2 class="text-3xl lg:text-5xl text-primary-600 font-bold">Our Work</h2>
-        <a href="#" class="backToTop">
+    <div class="flex justify-between">
+        <div>
+            <h2 class="text-3xl lg:text-5xl text-primary-600 font-bold">Our Work</h2>
+            <p class="mt-2 text-stone-400 text-xl">Explore a selection of projects we’ve delivered for our clients</p>
+        </div>
+        <a href="#" class="backToTop class text-primary-300 hover:text-primary-500">
             &#10514; Back to Top
         </a>
     </div>
@@ -10,15 +13,15 @@
     <div>
         <ul id="ourWork" class="flex space-x-12 overflow-x-auto">
             <li class="selected"><a href="">Countertops</a></li>
+            <li><a href="">Kitchens</a></li>
             <li><a href="">Walls</a></li>
-            <li><a href="">Granite</a></li>
-            <li><a href="">Ceramic</a></li>
-            <li><a href="">Porcelain</a></li>
+            <li><a href="">Washrooms</a></li>
+            <li><a href="">Basins</a></li>
         </ul>
     </div>
 </section>
 <section>
     <div>
-        <div id="visible-slide" class="mt-4 w-full aspect-video bg-cover bg-center bg-no-repeat bg-primary-50" style="background-image: url('/storage/img/unsplash-kitchen-homepage.jpg');"></div>
+        <div id="visible-slide" class="mt-4 w-full aspect-video bg-cover bg-center bg-no-repeat bg-primary-50" style="background-image: url('/storage/img/countertops.jpg');"></div>
     </div>
 </section>
