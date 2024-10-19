a62e6a09719fb9cdb6ece292d276f678f5d63c3a
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2024-10-19 17:56:16 +0000

    bug fix

--- resources/views/components/about-us.blade.php
+++ resources/views/components/about-us.blade.php
@@ -7,7 +7,7 @@
             </div>
             <p class="text-primary-800 text-lg max-w-prose mt-4 leading-loose">Since 2022, Ekow Stones has been transforming spaces with premium natural and artificial stone installations. Based in Ghana, we pride ourselves on delivering exceptional craftsmanship and attention to detail, ensuring seamless pattern alignment and quality finishes.</p>
             <div class="">
-                <a href="" class="inline-block mt-6 px-4 py-2 border bg-primary-50 items-center rounded-md hover:bg-primary-400 hover:text-primary-100">
+                <a href="#contactUsSection" class="contactUs inline-block mt-6 px-4 py-2 border bg-primary-50 items-center rounded-md hover:bg-primary-400 hover:text-primary-100">
                     <h4>Get in Touch</h4>
                 </a>
             </div>
