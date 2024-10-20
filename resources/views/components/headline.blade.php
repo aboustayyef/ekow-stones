bec701ffe2e4c03414e9511cb2876936199943e5
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2024-10-20 12:57:57 +0000

    improvements to mobile edition

--- resources/views/components/headline.blade.php
+++ resources/views/components/headline.blade.php
@@ -1,6 +1,6 @@
 <section>
     <div class="text-center text-2xl md:text-4xl lg:text-5xl font-bold">
        <h1 class="text-primary-700">Elevate your Space</h1>
-       <h2 class="mt-2 text-primary-400">With Timeless Stone</h2>
+       <h2 class="lg:mt-2 text-primary-400">With Timeless Stone</h2>
     </div>
  </section>
--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,9 +1,9 @@
-<section class="bg-primary-600 lg:bg-inherit">
-   <div>
-      <div class="lg:flex lg:h-[650px] overflow-hidden lg:rounded-3xl bg-primary-600 px-0">
+<section class="bg-primary-600 lg:bg-inherit pt-0 lg:pt-y">
+   <div class="px-0 lg:px-4">
+      <div class="flex flex-col-reverse lg:flex-row lg:h-[650px] overflow-hidden lg:rounded-3xl bg-primary-600 px-0">
          <div class="flex flex-col flex-shrink-0 justify-around lg:w-[475px] bg-primary-600 px-4 lg:p-12">
             <div>
-                <h3 class="text-honey-200 text-xl lg:text-2xl max-w-prose">Transform exceptional stones into elegant surfaces that are as distinctive as you are</h3>
+                <h3 class="text-honey-200 text-xl lg:text-2xl max-w-prose mt-6 lg:mt-0">Transform exceptional stones into elegant surfaces that are as distinctive as you are</h3>
                 <p class="mt-8 text-primary-100 text-lg lg:text-xl leading-relaxed max-w-prose">From marble and granite to quartz and porcelain, our expert craftsmanship ensures seamless surfaces with unmatched attention to detail, bringing elegance and durability to every project</p>
                 <div class="flex mt-8 space-x-4">
                     <a href="#ourWorkSection" class="seeOurWork block bg-honey-200 hover:bg-honey-100 active:bg-honey-300 rounded-lg py-2 px-4">See Our Work</a>
@@ -11,7 +11,8 @@
                 </div>
             </div>
         </div>
-         <img src="/storage/img/girl-near-wall.jpg" class="mt-6 lg:mt-0 rounded-3xl lg:rounded-none">
+        <div class="aspect-video lg:aspect-square bg-contain lg:bg-cover"  style="background-image: url('/storage/img/girl-near-wall.jpg');">
+        </div>
          {{-- <img src="/storage/img/unsplash-kitchen-homepage.jpg" class="mt-6 lg:mt-0 rounded-3xl lg:rounded-none"> --}}
       </div>
    </div>
