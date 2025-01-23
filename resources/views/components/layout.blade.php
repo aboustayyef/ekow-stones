719644e4afd5d32144df0bc957b676368abe08c6
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2025-01-23 13:31:46 +0000

    added metadata

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,3 +1,8 @@
+@php
+$meta_title = $title;
+$meta_description = "Ekow Stones: Premium natural and artificial stone installations in Ghana, delivering exceptional craftsmanship and quality finishes.";
+$meta_image = asset('/storage/img/ekow-stones-external-walls.jpg');
+@endphp
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,9 +15,18 @@
    <link rel="preconnect" href="https://fonts.googleapis.com">
    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;800&display=swap" rel="stylesheet">
+   <meta property="og:title" content="{{$meta_title}}">
+   <meta property="og:description" content="{{$meta_description}}">
+   <meta property="og:image" content="{{$meta_image}}">
+   <meta property="og:url" content="{{url()->current()}}">
+   <meta name="twitter:card" content="summary_large_image">
+   <meta name="twitter:title" content="{{$meta_title}}">
+   <meta name="twitter:description" content="{{$meta_description}}">
+   <meta name="twitter:image" content="{{$meta_image}}">
 </head>
 <body>
     {{$content}}
+
 
     @vite('resources/js/app.js')
 </body>
